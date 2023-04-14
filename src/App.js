cfa8ae2e93d0adaf456c7eb472c9afc44421bfe0
yair-byte <yairemilianocipres@gmail.com>
2023-04-14 20:48:16 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Link } from 'react-router-dom';
 import Home from './components/Home.jsx';
 import Gastos from './components/Gastos.jsx';
 import InputEntrante from './components/InputEntrante.jsx';
@@ -11,30 +11,30 @@ function App() {
 
   return (
     <>
-      <div className="container-fluid d-flex flex-column text-center font-responsive">
-        <nav className="navbar bg-primary navbar-expand-lg bg-body-tertiary bg-color">
-          <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-              <ul className="navbar-nav">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/home">Resumen</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/gastos">Gastos</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/entrantes">Entrantes</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>
-
       <BrowserRouter>
+        <div className="container-fluid d-flex flex-column text-center font-responsive">
+          <nav className="navbar bg-primary navbar-expand-lg bg-body-tertiary bg-color">
+            <div className="container-fluid">
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                  <li className="nav-item">
+                    <Link className="nav-link active" aria-current="page" to="/home">Resumen</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/gastos">Gastos</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/entrantes">Entrantes</Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/gastos" element={<Gastos />} />
